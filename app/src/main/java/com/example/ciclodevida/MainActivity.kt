49cbb1d6b551477abb0dc11ecbe231ebabfd405a
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-08-11 18:24:17 +0200

    OnRestart added

--- app/src/main/java/com/example/ciclodevida/MainActivity.kt
+++ app/src/main/java/com/example/ciclodevida/MainActivity.kt
@@ -12,41 +12,48 @@ class MainActivity : AppCompatActivity() {
     private var onPause = 0
     private var onStop = 0
     private var onDestroy = 0
+    private var onRestart = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Mi TAG", "onCreate $onCreate")
+        Log.d("MiTAG", "onCreate $onCreate")
         onCreate++
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("Mi TAG", "onStart $onStart")
+        Log.d("MiTAG", "onStart $onStart")
         onStart++
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("Mi TAG", "onResume $onResume")
+        Log.d("MiTAG", "onResume $onResume")
         onResume++
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("Mi TAG", "onPause $onPause")
+        Log.d("MiTAG", "onPause $onPause")
         onPause++
     }
 
     override fun onStop() {
-        Log.d("Mi TAG", "onStop $onStop")
+        Log.d("MiTAG", "onStop $onStop")
         onStop++
         super.onStop()
     }
 
     override fun onDestroy() {
-        Log.d("Mi TAG", "onDestroy $onDestroy")
+        Log.d("MiTAG", "onDestroy $onDestroy")
         onDestroy++
         super.onDestroy()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MiTAG", "onRestart $onRestart")
+        onRestart++
     }
 }
